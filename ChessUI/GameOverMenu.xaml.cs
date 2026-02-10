78e1f5a4772cd7f0a2b35a3c0aff3cd76963e29a
Agustin Scarafioca <Scarafioca@gmail.com>
2026-02-09 21:46:21 -0300

    GameOver part 2 with label

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -18,19 +19,59 @@ namespace ChessUI
     /// </summary>
     public partial class GameOverMenu : UserControl
     {
-        public GameOverMenu()
+
+        public event Action<Option> OptionSelected;
+
+        public GameOverMenu(GameState gameState)
         {
             InitializeComponent();
+
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
+        }
+
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "Gano las blancas",
+                Player.Black => "Ganan los negros",
+                _ => "Empate",
+            };
+        }
+
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "Blancas",
+                Player.Black => "Negras",
+                _ => "",
+            };
+        }
+
+        private static string GetReasonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Checkmate => $"El jugador {PlayerString(currentPlayer)} ha dado jaque mate",
+                EndReason.Stalemate => "Empate por ahogado",
+                EndReason.FiftyMoveRule => "Empate por regla de los 50 movimientos",
+                EndReason.ThreefoldRepetition => "Empate por triple repetición",
+                EndReason.InsufficientMaterial => "Empate por material insuficiente",
+                _ => "",
+            };
         }
 
         private void Restart_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Restart);
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected.Invoke(Option.Exit);
         }
     }
 }
